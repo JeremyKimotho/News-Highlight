064300613e480b7314d3f909f3231278526e7dae
Jeremy Kimotho <jeremykimotho@yahoo.com>
2018-11-09 15:20:08 +0300

    i have written the classes and tested their initialisation

--- tests/test_source.py
+++ tests/test_source.py
@@ -11,5 +11,8 @@ class SourceTest(unittest.TestCase):
     '''
     self.new_source = Source('Jeremy')
 
+  def test_instance(self):
+    self.assertTrue(isinstance(self.new_source, Source))
+
 if __name__ == '__main__':
   unittest.main()
