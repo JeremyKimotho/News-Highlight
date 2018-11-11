4951e1f9e11863681190086af64f147ee931da69
Jeremy Kimotho <jeremykimotho@yahoo.com>
2018-11-11 21:33:09 +0300

    i edited the base url  for the sources select because the toop-headlines were bringing a small amount of returns so i changed it to everything

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
 
   NEWS_API_BASE_URL = 'https://newsapi.org/v2/{}?country=us&apiKey={}'
-  SOURCE_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}' 
+  SOURCE_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}' 
   NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
   SECRET_KEY = os.environ.get('SECRET_KEY')
 
